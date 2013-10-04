e1744fd3c6914945d63cb4bf578e4b56293bc915
Clay Stewart <claytonmstewart@gmail.com>
2013-10-04 16:04:12 -0500

    listen for model changes and render the el in a new underscore template

--- src/models/stock.js
+++ src/models/stock.js
@@ -3,8 +3,6 @@
   window.Stock = Backbone.Model.extend({
     updatePrice: function (newPrice) {
       newPriceFloat = parseFloat(newPrice);
-      console.log('Updating', this.get('name'), 'price to:', newPriceFloat);
-      // TODO
       this.set({ price: newPriceFloat });
     }
   });
--- src/views/stock-view.js
+++ src/views/stock-view.js
@@ -1,8 +1,17 @@
 (function () {
 
+	var stockTemplateHtml = $('#templates .stock').html();
+	var stockTemplate = _.template(stockTemplateHtml);
+  
   window.StockView = Backbone.View.extend({
-    className: 'stock'
-    // TODO
+    className: 'stock',
+    initialize: function() {
+    	this.listenTo(this.model, 'change', this.render);
+    }, 
+    render: function() {
+			var newHtml = stockTemplate({ name: this.model.get("price"), price: this.model.get("name") });
+			$(this.el).html(newHtml);
+    }
   });
 
 })();
